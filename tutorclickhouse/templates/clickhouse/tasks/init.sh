0fa7075fb6618c7945b2d21dbc1e4931fa8e88fa
Cristhian Garcia <crisgarta8@gmail.com>
2023-05-08 15:01:39 -0500

    fix: add support for clickhouse cloud

--- tutorclickhouse/templates/clickhouse/tasks/init.sh
+++ tutorclickhouse/templates/clickhouse/tasks/init.sh
@@ -1,7 +1,7 @@
 echo "Initialising Clickhouse..."
 ch_connection_max_attempts=10
 ch_connection_attempt=0
-until clickhouse client --user {{ CLICKHOUSE_ADMIN_USER }} --password="{{ CLICKHOUSE_ADMIN_PASSWORD }}" --host "{{ CLICKHOUSE_HOST }}" --port {{ CLICKHOUSE_PORT }} -q 'exit'
+until clickhouse client --user {{ CLICKHOUSE_ADMIN_USER }} --password="{{ CLICKHOUSE_ADMIN_PASSWORD }}" --host "{{ CLICKHOUSE_HOST }}" {% if CLICKHOUSE_SECURE_CONNECTION %} --secure {% else %} --port {{ CLICKHOUSE_PORT }}{% endif %} -q 'exit'
 do
     ch_connection_attempt=$(expr $ch_connection_attempt + 1)
     echo "    [$ch_connection_attempt/$ch_connection_max_attempts] Waiting for Clickhouse service (this may take a while)..."
